95d730cbe2886fc2429e1d6b6d60e2b0e08dfad7
Anders Ugland <ugland.anders@gmail.com>
2020-05-22 13:36:20 +0200

    Add solution to all questions in module assignment 1

---
+++ script.js
@@ -0,0 +1,42 @@
+// question 1
+
+var firstVar;
+//let firstVar;
+//const firstVar;
+
+// question 2
+
+let name = "Anders";
+
+// question 3
+
+let num = 23;
+
+// question 4
+
+let division = 20 / 5;
+
+// question 5
+
+function checkType(something) {
+  return typeof something;
+}
+checkType("frog");
+
+// question 6 + 7
+
+let orderHasShipped = !true;
+
+if (orderHasShipped) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+//orderHasShipped ? "true" : "false";
+
+// question 8
+
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
